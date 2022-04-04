97e17e3cb5841a167648106f4a47f3973f61f016
Сергей Шевцов <78035367+Serg-Shevcov@users.noreply.github.com>
2022-04-04 23:12:38 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+let body = document.body;
+let url = window.location.href;
+let getName = (url) =>  {
+	let g = url.split('=');
+	let name = g[1];
+	if (name == undefined) {
+    name = 'Serg-Shevcov'
+	}
+	return name;
+}
+
+fetch(`https://api.github.com/users/${getName(url)}`)
+    .then(res => res.json())
+    .then(json => {
+        console.log(json.avatar_url);
+        console.log(json.name);
+        console.log(json.bio);
+        console.log(json.html_url);
+
+        let photo = new Image();
+        photo.src = json.avatar_url;
+        body.append(photo);
+
+        let name = document.createElement('p');
+        if (json.name != null) {
+            name.innerHTML = json.name;
+        } else {
+            name.innerHTML = 'Информация о пользователе недоступна';
+        }
+        body.append(name);
+        name.addEventListener("click", () => window.location = json.html_url);
+
+        let bio = document.createElement('p');
+        if (json.bio != null) {
+            bio.innerHTML = json.bio;
+        } else {
+            bio.innerHTML = 'Информация о пользователе недоступна';
+        }
+        body.append(bio);
+
+    })
+    .catch(err => alert('Информация о пользователе недоступна'));
